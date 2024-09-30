dae1b5038b85d5f61588c8295d71a0c0919257fe
behrouzsh <behrouzsharifi7@gmail.com>
2024-09-30 16:48:12 +0330

    widgets in forms

--- A/account/forms.py
+++ A/account/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class UserRegistrationForm(forms.Form):
-    username = forms.CharField(max_length=50, label='Username')
-    email = forms.EmailField()
-    password = forms.CharField(max_length=50, label='Password', widget=forms.PasswordInput)
+    username = forms.CharField(max_length=50, label='Username', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(max_length=50, label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'your password...'}))
