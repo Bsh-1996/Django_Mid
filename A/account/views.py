b8bc740ea24ecf334a4fd2c9d787fe9d568b2f8d
behrouzsh <behrouzsharifi7@gmail.com>
2024-10-07 20:50:06 +0330

    signals

--- A/account/views.py
+++ A/account/views.py
@@ -30,6 +30,7 @@ class UserRegisterView(View):
     def get(self, request):
         form = self.form_class()
         return render(request, self.template_name , {'form': form})
+    
     def post(self, request):
         form = self.form_class(request.POST)
         if form.is_valid():
